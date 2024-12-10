f4e8f51fdedddfdcb19e48ae6f71d685e163c833
Jack LoCasto <jack.a.locasto.civ@us.navy.mil>
2024-12-10 11:12:35 -0500

    Day2-part2 complete

--- day2/day2.py
+++ day2/day2.py
@@ -34,45 +34,38 @@ def is_safe_report(report: List[int]) -> Tuple[bool, int]:
     return True, None
 
 # Part 1
-def get_safe_report_count(reports: List[List[int]]) -> int:
-    safe_count = 0
+def get_safe_report_count(reports: List[List[int]]) -> Tuple[int, int]:
+    part1_safe_count = 0
+    part2_safe_count = 0
     for report in reports:
-        print("=========================")
         safe, bad_level = is_safe_report(report)
+        if safe:
+            part1_safe_count += 1
+        # Part 2 - Dampener
         # if deemed not safe at first, try removing the bad level or the level following the bad level and reevaluate
         if not safe:
-            print(report)
-            # print(f'Safe Before: {safe}')
             # remove bad level
             report_mut = report[:bad_level] + report[bad_level+1:]
-            print(report_mut)
             safe, _ = is_safe_report(report_mut)
             # remove level after bad level
             if not safe and (bad_level < len(report)):
                 report_mut = report[:bad_level+1] + report[bad_level+2:]
-                print(report_mut)
                 safe, _ = is_safe_report(report_mut)
             # remove level before bad level
             if not safe:
                 report_mut = report[:bad_level-1] + report[bad_level:]
-                print(report_mut)
                 safe, _ = is_safe_report(report_mut)
-            # print(f'Safe After: {safe}')
+            # remove first level
+            if not safe:
+                report_mut = report[1:]
+                safe, _ = is_safe_report(report_mut)
         
         if safe:
-            safe_count += 1
+            part2_safe_count += 1
             
-    return safe_count
+    return part1_safe_count, part2_safe_count
 
 if __name__ == "__main__":
     reports = read_inputs()
-    # reports = [
-    #     [7, 6, 4, 2, 1],
-    #     [1, 2, 7, 8, 9],
-    #     [9, 7, 6, 2, 1],
-    #     [1, 3, 2, 4, 5],
-    #     [8, 6, 4, 4, 1],
-    #     [1, 3, 6, 7, 9]
-    # ]
-    safe_report_count = get_safe_report_count(reports)
-    print(safe_report_count)
+    part1_safe_count, part2_safe_count = get_safe_report_count(reports)
+    print(f'Part1: {part1_safe_count}\nPart2: {part2_safe_count}')
